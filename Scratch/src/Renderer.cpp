fd19a86eaac63fa8041c54baffb8c2ed9d0e4527
Haidon Cosse <Haidonmcosse@gmail.com>
2024-10-29 17:01:11 -0500

    dd

--- Scratch/src/Renderer.cpp
+++ Scratch/src/Renderer.cpp
@@ -1,4 +1,5 @@
 #include "Renderer.h"
+#include <iostream>
 void GLClearError()
 {
     while (glGetError() != GL_NO_ERROR);
@@ -12,4 +13,18 @@ bool GLLogCall(const char* function, const char* file, int line)
         return false;
     }
     return true;
+}
+
+void Renderer::Clear() const
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void Renderer::Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const
+{
+    shader.Bind();
+    va.Bind();
+    ib.Bind();
+    GLCall(glDrawElements(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, nullptr));
+
 }
